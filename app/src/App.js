8c079859ab28bde539b591c784b938bca7f54bcc
ankit-suman-07 <ankitsuman07@gmail.com>
2023-11-01 01:07:45 +0530

    Frontend update test

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
     .get('https://server-gameit.onrender.com/games')
     .then((response) => {
       //setBooks(response.data.data);
-      console.log(response);
+      console.log(response.data);
       //setLoading(false);
     })
     .catch((error) => {
@@ -16,9 +16,31 @@ function App() {
       //setLoading(false);
     });
 
+  const update = () => {
+
+    const data = {
+      name: "new Game",
+      email: "abc.gmail.com",
+      password: "ewr43",
+      type: "user"
+    };
+    axios
+      .post('https://server-gameit.onrender.com/games', data)
+      .then((response) => {
+        //setBooks(response.data.data);
+        console.log(response.data);
+        //setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        //setLoading(false);
+      });
+  }
+
+
   return (
     <div className="App">
-
+      <button onClick={update} >Update</button>
     </div>
   );
 }
